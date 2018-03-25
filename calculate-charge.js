8fcc5e645551e58db7dabff05b0fb58bf9d035de
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-03-25 12:02:38 -0400

    Write function to calculate hours between start-time and bedtime

--- calculate-charge.js
+++ calculate-charge.js
@@ -5,9 +5,16 @@
 // Prompt end-time
 
 // Calculate hours between start-time and bedtime
+function startToBed(startTime, bedtime) {
+  return bedtime - startTime;
+}
 
 // Calculate hours between bedtime and midnight
 
 // Calculate hours between midnight and end-time
 
 // Calculate charge for each time period and print sum of charges.
+
+module.exports = {
+  startToBed: startToBed
+};
