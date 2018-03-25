c9ad46386ab729639d6391762acac56f639b1175
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-03-25 11:29:22 -0400

    Create script with pseudo code

---
+++ calculate-charge.js
@@ -0,0 +1,13 @@
+// Prompt start-time
+
+// Prompt bedtime
+
+// Prompt end-time
+
+// Calculate hours between start-time and bedtime
+
+// Calculate hours between bedtime and midnight
+
+// Calculate hours between midnight and end-time
+
+// Calculate charge for each time period and print sum of charges.
