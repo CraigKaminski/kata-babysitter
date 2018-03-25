505a48f863ab85b009d7f73dd525a103ddbba9d9
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-03-25 12:09:00 -0400

    Write test to check start-time to bedtime hours end at midnight if bedtime is after midnight

--- spec/calculate-charge.spec.js
+++ spec/calculate-charge.spec.js
@@ -7,4 +7,10 @@ describe('startToBed', function() {
     var bedtime = 8;
     expect(startToBed(startTime, bedtime)).toBe(3);
   });
+
+  it('Should return hours between start-time and midnight if bedtime is after midnight', function() {
+    var startTime = 5;
+    var bedtime = 1;
+    expect(startToBed(startTime, bedtime)).toBe(7);
+  });
 });
