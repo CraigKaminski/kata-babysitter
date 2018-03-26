eec8b4e7271d27119dc27ba87caa0cc794f9e327
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-03-26 15:55:16 -0400

    Refactor startToBed test to included combinations to make it easier to debug failing tests

--- spec/calculate-charge.spec.js
+++ spec/calculate-charge.spec.js
@@ -1,22 +1,59 @@
 var calculateCharge = require('../calculate-charge');
 var startToBed = calculateCharge.startToBed;
 
+/*
+s = start-time
+b = bedtime
+m = midnight
+e = end-time
+*/
+
 describe('startToBed', function() {
-  it('Should return hours between start-time and bedtime if start is before bedtime and bedtime is before midnight and end', function() {
-    var startTime = 5;
-    var bedtime = 8;
-    var endTime = 10
-    expect(startToBed(startTime, bedtime, endTime)).toBe(3);
-    var endTime = 2;
-    expect(startToBed(startTime, bedtime, endTime)).toBe(3);
+  describe('Should return hours between start-time and bedtime', function() {
+    it('for combination s b m e', function() {
+      var startTime = 5;
+      var bedtime = 8;
+      var endTime = 1;
+      expect(startToBed(startTime, bedtime, endTime)).toBe(3);
+    });
+    
+    it('for combination s b e m', function() {
+      var startTime = 5;
+      var bedtime = 9;
+      var endTime = 11;
+      expect(startToBed(startTime, bedtime, endTime)).toBe(4);
+    });
   });
 
-  it('Should return hours between start-time and midnight if start is before midnight and bedtime and end-time are after midnight', function() {
-    var startTime = 5;
-    var bedtime = 2;
-    var endTime = 2
-    expect(startToBed(startTime, bedtime, endTime)).toBe(7);
-    var endtime = 1
-    expect(startToBed(startTime, bedtime, endTime)).toBe(7);
+  describe('Should return hours between start-time and midnight', function() {
+    it('for combination s m b e', function() {
+      var startTime = 5;
+      var bedtime = 2;
+      var endTime = 3;
+      expect(startToBed(startTime, bedtime, endTime)).toBe(7);
+    });
+
+    it('for combiation s m e b', function() {
+      var startTime = 5;
+      var bedtime = 2;
+      var endTime = 1;
+      expect(startToBed(startTime, bedtime, endTime)).toBe(7);
+    });
+  });
+
+  describe('Should return hours between start-time and end-time', function() {
+    it('for combination s e m b', function() {
+      var startTime = 6;
+      var bedtime = 1;
+      var endTime = 10;
+      expect(startToBed(startTime, bedtime, endTime)).toBe(4);
+    });
+
+    it('for combination s e b m', function() {
+      var startTime = 6;
+      var bedtime = 11;
+      var endTime = 10;
+      expect(startToBed(startTime, bedtime, endTime)).toBe(4);
+    });
   });
 });
