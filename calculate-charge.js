faaae5df28a8c080f266c331616f2cc7957c77cb
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-03-26 15:07:14 -0400

    Add comment listing all valid combinations and call combinations which result in a non-zero start to bedtime

--- calculate-charge.js
+++ calculate-charge.js
@@ -23,3 +23,32 @@ function startToBed(startTime, bedtime) {
 module.exports = {
   startToBed: startToBed
 };
+
+/*
+All possible combinations where start is before end:
+(s = start, b = bedtime, m = midnight, e = end)
+s b m e
+s m b e
+b s m e
+b m s e
+m b s e
+m s b e
+s e m b
+s e b m
+s m e b
+s b e m
+b s e m
+m s e b
+*/
+
+/*
+All possible combinations where start is before end
+and start is before bedtime and midnight:
+(This is for calculating hours before bedtime)
+s b m e
+s b e m
+s m b e
+s m e b
+s e m b
+s e b m
+*/
