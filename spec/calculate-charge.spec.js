eb829b83aaa2feef60ce19abe668af24fd185439
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-03-26 15:22:02 -0400

    Update startToBed tests to cover two combinations where start-time is before bedtime and bedtime is before and and midnight. Also update startToBed tests to cover two combinations where start-time is before midnight and bedtime and end-time are after midnight.

--- spec/calculate-charge.spec.js
+++ spec/calculate-charge.spec.js
@@ -2,15 +2,21 @@ var calculateCharge = require('../calculate-charge');
 var startToBed = calculateCharge.startToBed;
 
 describe('startToBed', function() {
-  it('Should return hours between start-time and bedtime', function() {
+  it('Should return hours between start-time and bedtime if start is before bedtime and bedtime is before midnight and end', function() {
     var startTime = 5;
     var bedtime = 8;
-    expect(startToBed(startTime, bedtime)).toBe(3);
+    var endTime = 10
+    expect(startToBed(startTime, bedtime, endTime)).toBe(3);
+    var endTime = 2;
+    expect(startToBed(startTime, bedtime, endTime)).toBe(3);
   });
 
-  it('Should return hours between start-time and midnight if bedtime is after midnight', function() {
+  it('Should return hours between start-time and midnight if start is before midnight and bedtime and end-time are after midnight', function() {
     var startTime = 5;
-    var bedtime = 1;
-    expect(startToBed(startTime, bedtime)).toBe(7);
+    var bedtime = 2;
+    var endTime = 2
+    expect(startToBed(startTime, bedtime, endTime)).toBe(7);
+    var endtime = 1
+    expect(startToBed(startTime, bedtime, endTime)).toBe(7);
   });
 });
